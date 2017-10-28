536b01c740ea88eaa05419e5ecdd6b363271b8f6
davy <davy.wybiral@gmail.com>
2017-10-28 10:05:35 -0500

    Added POST handler

--- main.go
+++ main.go
@@ -16,15 +16,23 @@ func main() {
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler).Methods("GET")
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 	comments, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
 		return
 	}
 	templates.ExecuteTemplate(w, "index.html", comments)
+}
+
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush("comments", comment)
+	http.Redirect(w, r, "/", 302)
 }
