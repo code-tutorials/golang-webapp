5987173cbe3d9f733caa2ce2210ba06b254a725b
davy <davy.wybiral@gmail.com>
2017-11-10 13:05:04 -0600

    Added user model

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"fmt"
 	"errors"
 	"github.com/go-redis/redis"
 	"golang.org/x/crypto/bcrypt"
@@ -11,18 +12,65 @@ var (
 	ErrInvalidLogin = errors.New("invalid login")
 )
 
-func AuthenticateUser(username, password string) error {
-	hash, err := client.Get("user:" + username).Bytes()
-	if err == redis.Nil {
-		return ErrUserNotFound
-	} else if err != nil {
+type User struct {
+	key string
+}
+
+func NewUser(username string, hash []byte) (*User, error) {
+	id, err := client.Incr("user:next-id").Result()
+	if err != nil {
+		return nil, err
+	}
+	key := fmt.Sprintf("user:%d", id)
+	pipe := client.Pipeline()
+	pipe.HSet(key, "id", id)
+	pipe.HSet(key, "username", username)
+	pipe.HSet(key, "hash", hash)
+	pipe.HSet("user:by-username", username, id)
+	_, err = pipe.Exec()
+	if err != nil {
+		return nil, err
+	}
+	return &User{key}, nil
+}
+
+func (user *User) GetUsername() (string, error) {
+	return client.HGet(user.key, "username").Result()
+}
+
+func (user *User) GetHash() ([]byte, error) {
+	return client.HGet(user.key, "hash").Bytes()
+}
+
+func (user *User) Authenticate(password string) error {
+	hash, err := user.GetHash()
+	if err != nil {
 		return err
 	}
 	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
-	if err != nil {
+	if err == bcrypt.ErrMismatchedHashAndPassword {
 		return ErrInvalidLogin
 	}
-	return nil
+	return err
+}
+
+func GetUserByUsername(username string) (*User, error) {
+	id, err := client.HGet("user:by-username", username).Int64()
+	if err == redis.Nil {
+		return nil, ErrUserNotFound
+	} else if err != nil {
+		return nil, err
+	}
+	key := fmt.Sprintf("user:%d", id)
+	return &User{key}, nil
+}
+
+func AuthenticateUser(username, password string) error {
+	user, err := GetUserByUsername(username)
+	if err != nil {
+		return err
+	}
+	return user.Authenticate(password)
 }
 
 func RegisterUser(username, password string) error {
@@ -31,5 +79,6 @@ func RegisterUser(username, password string) error {
 	if err != nil {
 		return err
 	}
-	return client.Set("user:" + username, hash, 0).Err()
+	_, err = NewUser(username, hash)
+	return err
 }
